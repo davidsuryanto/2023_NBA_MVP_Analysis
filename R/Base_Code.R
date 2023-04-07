4a1dfc6698b375a3a316a690a43fef5ae1cce707
David Ariya Suryanto <david@Davids-MacBook-Air.local>
2023-04-06 18:37:08 -0700

    made some updates to the original datasets

--- R/Base_Code.R
+++ R/Base_Code.R
@@ -8,4 +8,31 @@ df <- read.csv('data/NBA_Dataset.csv')
 abb <- read.csv('data/abbreviations.csv')
 team_stats <- read.csv('data/renamed_teams.csv')
 df2 <- read.csv('data/cleaned_data.csv')
-stats_2023 <- read.csv('data/2023_NBA_Player_Stats.csv')
+player_stats_2023 <- read.csv('data/2023_NBA_Player_Stats.csv')
+
+# added a column of whether winning mvp that year
+df <- df %>%
+  group_by(season) %>%
+  mutate(mvp = award_share == max(award_share))
+
+# changed "Year" column name to "season"
+names(team_stats)[names(team_stats) == "Year"] <- "season"
+head(team_stats)
+
+# merge player data to team data
+team_stats <- merge(team_stats, abb, by = c("Team"))
+nba_data <- merge(team_stats, df, by = c("team_id", "season"))
+df <- merge(df, abb, by = c("team_id"))
+
+# add a column of whether the player has a double-double season 
+df <- df %>%
+  mutate(double_double = (pts_per_g >= 10 & ast_per_g >= 10) |
+           (pts_per_g >= 10 & trb_per_g >= 10) |
+           (pts_per_g >= 10 & stl_per_g >= 10) |
+           (pts_per_g >= 10 & blk_per_g >= 10) |
+           (ast_per_g >= 10 & trb_per_g >= 10) |
+           (ast_per_g >= 10 & stl_per_g >= 10) |
+           (ast_per_g >= 10 & blk_per_g >= 10) |
+           (trb_per_g >= 10 & stl_per_g >= 10) |
+           (trb_per_g >= 10 & blk_per_g >= 10) |
+           (stl_per_g >= 10 & blk_per_g >= 10))
